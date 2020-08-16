9cef7b8446a398cbd2b08c8f57e8f3a81a504d6c
bigpowermind <dirki.hrt.710@gmail.com>
2020-08-17 08:35:35 +0900

    slick-slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,15 @@
 //= require jquery
 //= require rails-ujs
+//= require_tree .
 
 $(function() {
   $('.Header__top__first-view').slick({
     // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
-    accessibility: true,
+    accessibility: false,
     // 自動再生。trueで自動再生される。
     autoplay: true,
     // 自動再生で切り替えをする時間
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     // 自動再生や左右の矢印でスライドするスピード
     speed: 400,
     // 切り替えのアニメーション。ease,linear,ease-in,ease-out,ease-in-out
