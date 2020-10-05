dc08dda7bd594d5a614d577be23658d31d83cf64
bigpowermind <dirki.hrt.710@gmail.com>
2020-10-06 04:11:37 +0900

    トップページのビューを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_group
+  
 
   # def index
   #   @comment = Comment.new
@@ -7,25 +7,22 @@ class CommentsController < ApplicationController
   # end
 
   def create
-    Comment.create(comment_params)
-    redirect_to content_path(@content)
+    comment = Comment.create(comment_params)
+    redirect_to content_path(current_user) #"/contents/#{comment.content.id}"
     # @comment = @content.comments.new(comment_params)
     # if @comment.save
     #   redirect_to content_path(@content), notice: "メッセージが送信されました"
     # else
     #   @comments = @content.comments.includes(:user)
     #   flash.now[:alert] = "メッセージを入力してください"
-    #   #render　送信できていない　コメントを送信するには？
     # end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text, :image).merge(user_id: current_user.id, content_id: params[:content_id])
+    params.require(:comment).permit(:comment, :image).merge(user_id: current_user.id, content_id: params[:content_id])
   end
 
-  def set_group
-    @content = Content.find(params[:content_id])
-  end
+  
 end
--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,13 +1,14 @@
 class ContentsController < ApplicationController
+  #before_action :set_group, only: :show
 
   def index
     @contents = Content.all
   end
 
   def show
-    @contents = Content.find(2)
-    @comment = Comment.new
-    @comments = @contents.comments.includes(:user)
+    @contents = Content.all
+    # @comment = Comment.new
+    # @comments = @content.comments.includes(:user)
   end
 
   
@@ -16,4 +17,13 @@ class ContentsController < ApplicationController
   def nut
     @contents = Content.all
   end
+
+  private
+
+  # def set_group
+  #   @content = Content.find(params[:id]).merge(user_id: current_user.id)
+  # end
 end
+
+# ログインが違えばコメントができなくなっている。
+# set_group,showアクションのid修正が必要！！
