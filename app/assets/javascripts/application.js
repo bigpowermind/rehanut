d295d6954dd85a7195b5cddb42b30b641b7b4125
bigpowermind <dirki.hrt.710@gmail.com>
2020-11-15 04:24:18 +0900

    リファクタリング

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,3 @@
 //= require jquery
 //= require rails-ujs
 //= require_tree .
-
-$(function() {
-  $('.Main__top__first-view').slick({
-    // アクセシビリティ。左右ボタンで画像の切り替えをできるかどうか
-    accessibility: false,
-    // 自動再生。trueで自動再生される。
-    autoplay: true,
-    // 自動再生で切り替えをする時間
-    autoplaySpeed: 3000,
-    // 自動再生や左右の矢印でスライドするスピード
-    speed: 400,
-    // 切り替えのアニメーション。ease,linear,ease-in,ease-out,ease-in-out
-    cssEase: 'ease',
-    // 画像下のドット（ページ送り）を表示
-    dots: false,
-    // ドットのclass名をつける
-    dotsClass: 'dot-class',
-    // 左右の次へ、前へボタンを表示するかどうか
-    arrows: true,
-    // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
-    infinite: true,
-  });
-});
